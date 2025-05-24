da7fa4d7653f1da07bb6374d89fb3d5fab6a3be0
sandilsranasinghe <sandilsranasinghe@gmail.com>
2025-05-24 17:57:41 +0530

    add extra function foo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # this will be our main file
 
+def foo_function():
+    print("This is an extra function that can be used in other modules.")
+
 def main():
     print("This is the main function.")
     print("You can run this script directly or import it as a module.")
@@ -7,3 +10,4 @@ def main():
 if __name__ == "__main__":
     print("Hello, World!")
     main()
+    foo_function()
