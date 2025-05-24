4c32153e26ab9206008c83b12d3190a30f6f56f3
sandilsranasinghe <sandilsranasinghe@gmail.com>
2025-05-24 17:53:19 +0530

    add main func

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # this will be our main file
 
+def main():
+    print("This is the main function.")
+    print("You can run this script directly or import it as a module.")
+
 if __name__ == "__main__":
     print("Hello, World!")
+    main()
